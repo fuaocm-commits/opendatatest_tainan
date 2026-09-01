05a4e8fc0d9ecf7f107f0de12a6ea311eaa41531
fuaocm-commits <fuaocm@gmail.com>
2026-09-01 16:09:33 +0800

    remove env

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ load_dotenv()
 
 #2. 設定log 機制(純寫到file &輸出到終端機上)
 logging.basicConfig(
-    level=logging. INFO #設定記錄層級
-    fo
-
+    level=logging. INFO, # 設定記錄層級
+    format='%(asctime)s [%(lenelname)s] %(filename)s(f]:%(lineno)d: %(message)s)',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler("scraper.log", encoding="utf-8"),
+        logging.StreamHandler()
+        ]
 )
+
+#設定變數
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_USER = os.environ.get("DB_USER", "peter")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "[PASSWORD]")
+DB_PORT = int(os.environ.get("DB_PORT", 3306))
+DB_NAME = os.environ.get("DB_NAME", "tainan")
+DB_CHARSET = os.environ.get("DB_CHARSET", "utf8mb4")
+
+API_URL = os.environ.get("API_URL")
+EXCEL_FILENAME = os.environ.get("EXCEL_FILENAME", "tainan_house.xlsx")
